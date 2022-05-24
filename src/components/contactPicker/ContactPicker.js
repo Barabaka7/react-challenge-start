126d17be3bb6ed1c8fd8797d192bfa1381db12f3
Barabaka <ivan.gopey@gmail.com>
2022-05-24 13:52:13 +0300

    Fix Warnings in ContactPicker

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export const ContactPicker = ({contacts, onChange}) => {
   return (
-    <select onChange={onChange}>
-      <option selected="selected">Nothing selected</option>
-      { contacts.map( i => <option>{i.name}</option>)}
+    <select onChange={onChange} defaultValue={"DEFAULT"}>
+      <option key={"id-"+0} value="DEFAULT" disabled>Nothing selected</option>
+      { contacts.map( (i,j) => <option key={"id-"+j+1} value={i.name}>{i.name}</option>)}
     </select>
   );
 };
